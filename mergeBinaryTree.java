b0b06d01765a893847ce68c6923d14c2dea93ea4
Claire <34062660+claire-maria@users.noreply.github.com>
2018-11-05 18:53:06 +0000

    Update mergeBinaryTree.java

--- mergeBinaryTree.java
+++ mergeBinaryTree.java
@@ -6,6 +6,20 @@
  *     TreeNode right;
  *     TreeNode(int x) { val = x; }
  * }
+ Input: 
+	Tree 1                     Tree 2                  
+          1                         2                             
+         / \                       / \                            
+        3   2                     1   3                        
+       /                           \   \                      
+      5                             4   7                  
+Output: 
+Merged tree:
+	     3
+	    / \
+	   4   5
+	  / \   \ 
+	 5   4   7
  */
 class Solution {
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
